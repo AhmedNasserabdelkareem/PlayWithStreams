c3fcb6e0c4d6c12d4257637075263b42fb134466
Ahmed Nasser <ahmednasser2337@gmail.com>
2019-06-01 10:18:52 +0200

    Delete Unused data

--- stream/src/stream/WebsocketController.java
+++ stream/src/stream/WebsocketController.java
@@ -1,14 +1,17 @@
 package stream;
 
 import java.net.URI;
+
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/endpoint")
 public class WebsocketController {
 	private String Query;
 	private ArrayList<String> data;
-	
 	public ArrayList<String> getData() {
 		return data;
 	}
@@ -16,22 +19,20 @@ public class WebsocketController {
 	public WebsocketController(String query) {
 		super();
 		Query = query;
+		data = new ArrayList<>();
 		start();
 	}
 
 	private LinkedList<String> start() {
-		data = new ArrayList<>();
    	 try {
          final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI(Query));
          clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
              public void handleMessage(String message) {
-            	 data.add(message);
-                // System.out.println(message);
+            	 data.add(message);            	 
+                 System.out.println(message);
              }
          });
-
-         clientEndPoint.sendMessage("start");
-         Thread.sleep(1000);
+         Thread.sleep(Long.MAX_VALUE);
 
      } catch (InterruptedException ex) {
          System.err.println("InterruptedException exception: " + ex.getMessage());
--- stream/src/stream/main.java
+++ stream/src/stream/main.java
@@ -1,17 +1,15 @@
 package stream;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/endpoint")
+
 public class main {
 	final static String LOCAL_HOST = "ws://localhost:9000/ws";
     public static void main(String[] args) {
     	WebsocketController c = new WebsocketController(LOCAL_HOST); 
     	JsonParser parser = new JsonParser(c.getData());
     	Visualizer v = new Visualizer (parser.trips);
-    //	printTrips(c.getData());
+//    	printTrips(c.getData());
   }
      
     static void printTrips(ArrayList<String> x){
