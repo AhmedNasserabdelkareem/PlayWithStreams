1d8a0ed8563c71ffe4196c5768ec832671d5388f
Ahmed Nasser <ahmednasser2337@gmail.com>
2019-06-09 12:11:06 +0200

    Final Controller

--- stream/src/stream/WebsocketController.java
+++ stream/src/stream/WebsocketController.java
@@ -1,5 +1,8 @@
 package stream;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 
 import java.net.URISyntaxException;
@@ -12,132 +15,181 @@ import javax.websocket.server.ServerEndpoint;
 
 import Gui.DashBoard;
 
+/**
+ * @author Ahmednasser
+ *
+ */
+
 @ServerEndpoint("/endpoint")
 public class WebsocketController {
 	private static final int MONTH_LENGTH = 32;
 	private static final int YEAR_LENGTH = 13;
+	private static final int TAXI_TYPES_NUMBER = 3;
 	private String Query;
-	private ArrayList<Trip> [][] data;
-	private JsonParser parser ;
-	int [] avgMinTrips = new int[3];
-	int [] nonDropOff = new int[3];
-	int [] MadBrok = new int[3];
-	int GlobalMonth;
-	Set<String> UniqueVehicles [] = new HashSet [32];
-	String MADBROOCLocationID ="";
-	TripDauration [] tripTime = new TripDauration[3];
+	private ArrayList<Trip>[][] data;
+	private JsonParser parser;
+	int[] avgMinTrips = new int[TAXI_TYPES_NUMBER];
+	int[] nonDropOff = new int[TAXI_TYPES_NUMBER];
+	int[] MadBrok = new int[TAXI_TYPES_NUMBER];
+	int GlobalMonth, Trips_Count = 0, Failure_Count = 0;
+	Set<String> UniqueVehicles[] = new HashSet[MONTH_LENGTH];
+	Set<String> Vehicles = new HashSet<>();
+	String MADBROOCLocationID = "", WoodsideQueensLocationID = "";
+	int[] WoodsideTrips = new int[TAXI_TYPES_NUMBER];
+	TripDauration[] tripTime = new TripDauration[TAXI_TYPES_NUMBER];
 	ArrayList<Location> locations;
-	public ArrayList<Trip> [][] getData() {
-		return data;
-	}
+	PrintWriter writer;
 
-	public WebsocketController(String query,int globalMonth ,ArrayList<Location> loc) {
+	public WebsocketController(String query, int globalMonth, ArrayList<Location> loc) {
 		super();
 		Query = query;
-		data = new ArrayList[13][32];
-		locations=loc;
-		GlobalMonth=globalMonth;
+		data = new ArrayList[YEAR_LENGTH][MONTH_LENGTH];
+		locations = loc;
+		GlobalMonth = globalMonth;
 		parser = new JsonParser();
 		DashBoard.main();
 		initializeData();
-		getMadBrock();
+		setImportantLocationIDs();
 		start();
 	}
 
 	private void initializeData() {
-		for(int i=0;i<YEAR_LENGTH;i++)	{
-		for (int j = 0; j < MONTH_LENGTH; j++) {
+		for (int i = 0; i < YEAR_LENGTH; i++) {
+			for (int j = 0; j < MONTH_LENGTH; j++) {
 				data[i][j] = new ArrayList<Trip>();
-			
-		}}
-		for (int i=0;i<3;i++){
-			avgMinTrips[i]=0;
-			nonDropOff[i]=0;
-			MadBrok[i]=0;
-			tripTime[i] = new TripDauration();
+			}
 		}
-		for(int i=0;i<MONTH_LENGTH;i++){
-			UniqueVehicles[i]= new HashSet<>();
+		for (int i = 0; i < TAXI_TYPES_NUMBER; i++) {
+			avgMinTrips[i] = 0;
+			nonDropOff[i] = 0;
+			MadBrok[i] = 0;
+			tripTime[i] = new TripDauration();
+			WoodsideTrips[i] = 0;
+		}
+		for (int i = 0; i < MONTH_LENGTH; i++) {
+			UniqueVehicles[i] = new HashSet<>();
 		}
 	}
 
 	private LinkedList<String> start() {
-   	 try {
-         final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI(Query));
-         clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
-             public void handleMessage(String message) {
-            	 Trip t = parser.setTripData(message);
-               	 int day = t.dropDate.getDate();
- 				 data[GlobalMonth][day].add(t);
-            	 UpdateGUI(t,day);
+		try {
+			final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI(Query));
+			clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
+				public void handleMessage(String message) {
+					Trip t = parser.setTripData(message);
+					if (t.DriverId.length() == 0 || t.taxi.length() == 0) {
+						Failure_Count++;
+					} else {
+						int day = t.dropDate.getDate();
+						data[GlobalMonth][day].add(t);
+						UpdateGUI(t, day);
+						writeResults();
+						Trips_Count++;
+					}
+				}
+			});
+			Thread.sleep(Long.MAX_VALUE);
 
-             }
-         }
-         
-         );
-         Thread.sleep(Long.MAX_VALUE);
-
-     } catch (InterruptedException ex) {
-         System.err.println("InterruptedException exception: " + ex.getMessage());
-     } catch (URISyntaxException ex) {
-         System.err.println("URISyntaxException exception: " + ex.getMessage());
-     }
+		} catch (InterruptedException ex) {
+			System.err.println("InterruptedException exception: " + ex.getMessage());
+		} catch (URISyntaxException ex) {
+			System.err.println("URISyntaxException exception: " + ex.getMessage());
+		}
 		return null;
 	}
 
-	private void getMadBrock() {
-		for (int i=0;i<locations.size();i++){
-			if(locations.get(i).zone.equals("Madison")&&locations.get(i).borough.equals("Brooklyn")){
-			MADBROOCLocationID=locations.get(i).id;
-		}}
+	private void setImportantLocationIDs() {
+		for (int i = 0; i < locations.size(); i++) {
+			if (locations.get(i).zone.equals("Madison") && locations.get(i).borough.equals("Brooklyn")) {
+				MADBROOCLocationID = locations.get(i).id;
+			}
+
+			if (locations.get(i).zone.equals("Woodside") && locations.get(i).borough.equals("Queens")) {
+				WoodsideQueensLocationID = locations.get(i).id;
+			}
+		}
 	}
-	
-	private void UpdateGUI (Trip t,int day){
-   	 UniqueVehicles[day-1].add(t.DriverId);
-		 int tripGui = Integer.parseInt(DashBoard.totalTrips[day-1].getText().toString())+1;
-		 DashBoard.totalTrips[day-1].setText(String.valueOf(tripGui));
-		 DashBoard.Vehicles[day-1].setText(String.valueOf(UniqueVehicles[day-1].size()));
-		 String type =t.taxi;
-			if (t.pickLocId==MADBROOCLocationID) {
+
+	private void UpdateGUI(Trip t, int day) {
+		UniqueVehicles[day - 1].add(t.DriverId);
+		Vehicles.add(t.DriverId);
+		int tripGui = Integer.parseInt(DashBoard.totalTrips[day - 1].getText().toString()) + 1;
+		DashBoard.totalTrips[day - 1].setText(String.valueOf(tripGui));
+		DashBoard.Vehicles[day - 1].setText(String.valueOf(UniqueVehicles[day - 1].size()));
+		String type = t.taxi;
+		if (t.pickLocId == MADBROOCLocationID) {
 			switch (type) {
-				case Titles.GREEN:
-					 int greenMadBroc = Integer.parseInt(DashBoard.MadBroc[day-1][1].getText().toString())+1;
-					 DashBoard.MadBroc[day-1][1].setText(String.valueOf(greenMadBroc));
-				case Titles.YELLOW:
-					 int yellowMadbroc = Integer.parseInt(DashBoard.MadBroc[day-1][0].getText().toString())+1;
-					 DashBoard.MadBroc[day-1][0].setText(String.valueOf(yellowMadbroc));
-				case Titles.FHV:
-					 int fhvMadBroc = Integer.parseInt(DashBoard.MadBroc[day-1][2].getText().toString())+1;
-					 DashBoard.MadBroc[day-1][2].setText(String.valueOf(fhvMadBroc));
+			case Titles.GREEN:
+				int greenMadBroc = Integer.parseInt(DashBoard.MadBroc[day - 1][1].getText().toString()) + 1;
+				DashBoard.MadBroc[day - 1][1].setText(String.valueOf(greenMadBroc));
+			case Titles.YELLOW:
+				int yellowMadbroc = Integer.parseInt(DashBoard.MadBroc[day - 1][0].getText().toString()) + 1;
+				DashBoard.MadBroc[day - 1][0].setText(String.valueOf(yellowMadbroc));
+			case Titles.FHV:
+				int fhvMadBroc = Integer.parseInt(DashBoard.MadBroc[day - 1][2].getText().toString()) + 1;
+				DashBoard.MadBroc[day - 1][2].setText(String.valueOf(fhvMadBroc));
 			}
+		}
+
+		if (t.dropLocId.length() == 0) {
+			switch (type) {
+			case Titles.GREEN:
+				int NDGREEN = Integer.parseInt(DashBoard.NDGreen.getText().toString()) + 1;
+				DashBoard.NDGreen.setText(String.valueOf(NDGREEN));
+			case Titles.YELLOW:
+				int NDYELLOW = Integer.parseInt(DashBoard.NDYellow.getText().toString()) + 1;
+				DashBoard.NDYellow.setText(String.valueOf(NDYELLOW));
+			case Titles.FHV:
+				int NDFHV = Integer.parseInt(DashBoard.NDFhv.getText().toString()) + 1;
+				DashBoard.NDFhv.setText(String.valueOf(NDFHV));
 			}
-			if(t.dropLocId.length()==0){
-				switch (type){
-					case Titles.GREEN : 
-					 int NDGREEN = Integer.parseInt(DashBoard.NDGreen.getText().toString())+1;
-					 DashBoard.NDGreen.setText(String.valueOf(NDGREEN));
-					case Titles.YELLOW :
-						int NDYELLOW = Integer.parseInt(DashBoard.NDYellow.getText().toString())+1;
-						 DashBoard.NDYellow.setText(String.valueOf(NDYELLOW));
-					case Titles.FHV : 
-						int NDFHV = Integer.parseInt(DashBoard.NDFhv.getText().toString())+1;
-						 DashBoard.NDFhv.setText(String.valueOf(NDFHV));
-				}
+		}
+
+		switch (type) {
+		case Titles.GREEN:
+			tripTime[0].totalTrips++;
+			tripTime[0].totalTime += t.Tripdauration;
+			DashBoard.MinGreen.setText(String.format("%.2f", tripTime[0].totalTime / tripTime[0].totalTrips));
+		case Titles.YELLOW:
+			tripTime[1].totalTrips++;
+			tripTime[1].totalTime += t.Tripdauration;
+			DashBoard.MinYellow.setText(String.format("%.2f", tripTime[1].totalTime / tripTime[1].totalTrips));
+		case Titles.FHV:
+			tripTime[2].totalTrips++;
+			tripTime[2].totalTime += t.Tripdauration;
+			DashBoard.MinFhv.setText(String.format("%.2f", tripTime[2].totalTime / tripTime[2].totalTrips));
+
+		}
+		if (t.pickLocId == WoodsideQueensLocationID) {
+			switch (type) {
+			case Titles.GREEN:
+				WoodsideTrips[0]++;
+			case Titles.YELLOW:
+				WoodsideTrips[1]++;
+			case Titles.FHV:
+				WoodsideTrips[2]++;
 			}
-			
-			switch (type){
-			case Titles.GREEN : 
-				tripTime[0].totalTrips++;tripTime[0].totalTime+=t.Tripdauration;
-				DashBoard.MinGreen.setText(String.format("%02d", tripTime[0].totalTime/tripTime[0].totalTrips));
-			case Titles.YELLOW : 
-				tripTime[1].totalTrips++;tripTime[1].totalTime+=t.Tripdauration;
-				DashBoard.MinYellow.setText(String.format("%02d", tripTime[1].totalTime/tripTime[1].totalTrips));
-			case Titles.FHV : 
-				tripTime[2].totalTrips++;tripTime[2].totalTime+=t.Tripdauration;
-				DashBoard.MinFhv.setText(String.format("%02d", tripTime[2].totalTime/tripTime[2].totalTrips));
-				
-    }
+		}
 	}
 
+	public ArrayList<Trip>[][] getData() {
+		return data;
+	}
 
+	private void writeResults() {
+		try {
+			writer = new PrintWriter("..\\results\\result.txt", "UTF-8");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		writer.println("Total number of received records : " + String.valueOf(Trips_Count + Failure_Count));
+		writer.println("Total number of trips : " + String.valueOf(Trips_Count));
+		writer.println("Average trips per day the given month : " + String.format("%.2f", Trips_Count / 31.0));
+		writer.println("Total number of distinct vehicles : " + MONTH_LENGTH);
+		writer.println("Total number of trips picked up from Woodside,Queens :" + "Green( " + WoodsideTrips[0]
+				+ " ), Yellow( " + WoodsideTrips[1] + " ), FHV( " + WoodsideTrips[2] + " )");
+		writer.close();
+	}
 }
