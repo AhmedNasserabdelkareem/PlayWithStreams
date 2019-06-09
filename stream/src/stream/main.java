716b694ab47aa7be1ace02d3bc0c6fba360d8023
Ahmed Nasser <ahmednasser2337@gmail.com>
2019-06-09 12:30:06 +0200

    Adding Main test

--- stream/src/stream/main.java
+++ stream/src/stream/main.java
@@ -1,13 +1,15 @@
 package stream;
 
-import java.util.ArrayList;
-
+/**
+ * @author Ahmednasser
+ *
+ */
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	    LocationsReader r = new LocationsReader("taxi_zones_simple.csv");
-		WebsocketController c = new WebsocketController("ws://localhost:9000/ws",11, r.getLocations());
+		LocationsReader locationsReader = new LocationsReader("taxi_zones_simple.csv");
+		WebsocketController controller = new WebsocketController("ws://localhost:9000/ws", 11,
+				locationsReader.getLocations());
 
 	}
 
